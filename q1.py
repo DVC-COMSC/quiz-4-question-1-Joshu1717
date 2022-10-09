5ac67777625abc352135caf8a84940bcaf1df84b
Kyu Lee <kwooler@gmail.com>
2022-10-09 16:53:34 -0700

    q4

--- q1.py
+++ q1.py
@@ -10,22 +10,24 @@
 ###
 
 
-list = [1, 3, 17, 9, 15, 2, 4, 1, 10, 12]
-next = 1 
+# list = [1, 3, 17, 9, 15, 2, 4, 1, 10, 12]
+next = 1
 count = 0
+
+list = list(map(int, input().split()))
 
 
 for num in list[1:]:
     odd_count = 0
     even_count = 0
     if num % 2 == 0:
-        if next % 2 ==0:
+        if next % 2 == 0:
             even_count += 1
-            next += 1 
+            next += 1
     elif num % 2 != 0:
         if next % 2 != 0:
-           odd_count += 1
-           next += 1 
+            odd_count += 1
+            next += 1
     else:
-        next +=1
-    print(even_count, odd_count)        
+        next += 1
+    print(even_count, odd_count)
